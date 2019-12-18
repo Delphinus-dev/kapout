761b31d47b6421d3cb5affaf5bbb3d62dc8d4ea9
Monsieur486 <monsieur486@gmail.com>
2019-12-18 14:51:20 +0100

    first commit

--- push.sh
+++ push.sh
@@ -2,10 +2,10 @@
 php composer.phar self-update
 php composer.phar install
 yarn install
-php bin/console translation:update --dump-messages fr --domain=messages
-php bin/console translation:update --dump-messages en --domain=messages
-php bin/console translation:update --force fr --domain=messages
-php bin/console translation:update --force en --domain=messages
+#php bin/console translation:update --dump-messages fr --domain=messages
+#php bin/console translation:update --dump-messages en --domain=messages
+#php bin/console translation:update --force fr --domain=messages
+#php bin/console translation:update --force en --domain=messages
 php bin/console cache:clear
 yarn encore dev
 php bin/console make:migration
