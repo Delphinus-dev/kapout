2ba60b9d6981af2bc0a506782a30de4a13697154
corinne <cdallongeville22@gmail.com>
2019-12-18 19:22:17 +0100

    c'est mon premier hihihi

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,10 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+require('../scss/app.scss');
+// require the JavaScript
+require('bootstrap');
+
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
