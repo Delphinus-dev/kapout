9e6a28155d1b6ae715ac010e5c726eeaed75dcce
Delphinus_dev <delphine.gobbin@gmail.com>
2019-12-18 19:35:26 +0100

    début classe Santa

--- src/Service/Santa.php
+++ src/Service/Santa.php
@@ -1,10 +1,21 @@
 <?php
 
-
 namespace App\Service;
-
 
 class Santa
 {
-    CONST
+    const ID = 732;
+    const NAME = "Santa Claus";
+    const ALIAS = "Baba Noel";
+    const BIRTHPLACE = "Patar, Lycia, Turkey";
+    const LOVER = "Rudolph";
+    const ALIGNMENT = "good";
+
+    function echoName()
+    {
+        echo self::NAME;
+    }
 }
+
+$instance = new Santa;
+$instance->echoName();
