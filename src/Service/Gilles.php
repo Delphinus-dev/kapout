549a3b2f305e2650c49a678a5edc3e1e899a904a
Delphinus_dev <delphine.gobbin@gmail.com>
2019-12-19 00:14:37 +0100

    Classes Santa Yavuz et Gilles

--- src/Service/Gilles.php
+++ src/Service/Gilles.php
@@ -7,10 +7,18 @@ class Gilles
 {
     const ID = 346; // ID DE IRONMAN
     const NAME = "Gilles Samuel";
-    const QUOTE1 = "Best campus manager";
-    const QUOTE2 = "Hamilton";
-    const QUOTE3 = "";
-    const LIE = "";
+
+    const QUOTE_1 = "Best campus manager ever";
+    const QUOTE_2 = "Hamilton";
+    const QUOTE_3 = "A fait les Beaux Arts";
+    const QUOTE_4 = "Université de Chicoutimi";
+    const QUOTE_5 = "Nike";
+    const QUOTE_6 = "Wild code doggo Pika";
+
+    const LIE_1 = "A été 1er à un concours de smurf à l'école primaire";
+    const LIE_2 = "Déteste les figurines Pop";
+    const LIE_3 = "Moi moche et méchant, c'est bon";
+
     const IMAGE = "https://www.superheroapi.com/api.php/2608451795899942/346/image";
 
     function echoName()
--- src/Service/Yavuz.php
+++ src/Service/Yavuz.php
@@ -6,10 +6,18 @@ class Yavuz
 {
     const ID = 332; // ID DE HULK
     const NAME = "Yavuz Kutuk";
-    const QUOTE1 = "Je vous aime";
-    const QUOTE2 = "On peut négocier";
-    const QUOTE3 = "I ♡ KEBAB";
-    const LIE = "Il est TOP";
+
+    const QUOTE_1 = "Je vous aime";
+    const QUOTE_2 = "On peut négocier";
+    const QUOTE_3 = "I ♥ KEBAB";
+    const QUOTE_4 = "On vise l'EXCELLENCE";
+    const QUOTE_5 = "JS c'est de la merde";
+    const QUOTE_6 = "Laravel";
+
+    const LIE_1 = "Je suis TOP";
+    const LIE_2 = "J'adore faire du front";
+    const LIE_3 = "Son habilitation en tant que juré Afpa expire en décembre 2022";
+
     const IMAGE = "https://www.superheroapi.com/api.php/2608451795899942/332/image";
 
     function echoName()
