136021cc85f5b89cfe83e2770d931e93d9721ab3
Monsieur486 <monsieur486@gmail.com>
2019-12-19 02:15:08 +0100

    Affichage Mercure

--- src/Controller/BackOfficeController.php
+++ src/Controller/BackOfficeController.php
@@ -5,9 +5,6 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mercure\Publisher;
-use Symfony\Component\Mercure\Update;
 
 class BackOfficeController extends AbstractController
 {
@@ -17,23 +14,5 @@ class BackOfficeController extends AbstractController
     public function backoffice()
     {
         return $this->render('backOfficePage/index.html.twig');
-    }
-
-    /**
-     * @Route("/godata", name="sendData")
-     * @param Publisher $publisher
-     * @return Response
-     */
-    public function __invoke(Publisher $publisher): Response
-    {
-        $update = new Update(
-            'questions',
-            json_encode(['status' => 'OutOfStock'])
-        );
-
-        // The Publisher service is an invokable object
-        $publisher($update);
-
-        return new Response('published!');
     }
 }
