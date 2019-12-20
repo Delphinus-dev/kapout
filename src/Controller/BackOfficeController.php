a9485c8250ffe050555067ed52bb01c6951b43b5
Monsieur486 <monsieur486@gmail.com>
2019-12-20 13:21:12 +0100

    Registration Form

--- src/Controller/BackOfficeController.php
+++ src/Controller/BackOfficeController.php
@@ -6,7 +6,6 @@ namespace App\Controller;
 use App\Entity\Reponse;
 use App\Entity\User;
 use App\Service\ApiGet;
-use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
