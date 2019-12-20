582f6d53697fa12052467961d84c62efbe9e19aa
Delphinus_dev <delphine.gobbin@gmail.com>
2019-12-20 03:53:27 +0100

    table reponse

---
+++ src/Entity/Reponse.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ReponseRepository")
+ */
+class Reponse
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $question;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $reponse;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuestion(): ?int
+    {
+        return $this->question;
+    }
+
+    public function setQuestion(int $question): self
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    public function getReponse(): ?int
+    {
+        return $this->reponse;
+    }
+
+    public function setReponse(int $reponse): self
+    {
+        $this->reponse = $reponse;
+
+        return $this;
+    }
+}
