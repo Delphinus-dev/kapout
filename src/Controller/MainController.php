8e9cb2094e21ef124658b564ef18b4597ab4e2bd
Delphinus_dev <delphine.gobbin@gmail.com>
2019-12-20 05:36:42 +0100

    mise à jour entité Réponse

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Service\ApiGet;
-use App\Service\ReplaceDash;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -32,50 +31,11 @@ class MainController extends AbstractController
     }
 
     /**
-     * @Route("/questions", name="questionPage")
+     * @Route("/game" , name="game")
      * @return Response
-     * @throws ClientExceptionInterface
-     * @throws DecodingExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws TransportExceptionInterface
      */
-    public function question()
+    public function routeGame()
     {
-        $apiGet = (new \App\Service\ApiGet)->getApi();
-        dump($apiGet);
-        $i = 0;
-            foreach ($apiGet as $key => $value){
-                $getQuestion[$i]['fullname'] = $apiGet[$key]['biography']['full-name'];
-                $getQuestion[$i]['gender'] = $apiGet[$key]['appearance']['gender'];
-                $getQuestion[$i]['alignment'] = $apiGet[$key]['biography']['alignment'];
-                $getQuestion[$i]['race'] = $apiGet[$key]['appearance']['race'];
-                $getQuestion[$i]['eye-color'] = $apiGet[$key]['appearance']['eye-color'];
-                $getQuestion[$i]['base'] = $apiGet[$key]['work']['base'];
-                $getQuestion[$i]['occupation'] = $apiGet[$key]['work']['occupation'];
-                $getQuestion[$i]['publisher'] = $apiGet[$key]['biography']['publisher'];
-                    foreach ($getQuestion[$i] as $k2 => $v2) {
-                        if (
-                            $v2 == 'null' ||
-                            $v2 == "-" ||
-                            $v2 == "") {
-
-                            unset($getQuestion[$i][$k2]);
-                        }
-                    }
-                $i++;
-            }
-            json_encode($getQuestion, true);
-        // foreach pour faire sauter les entrées vides
-        // foreach shufflisation des données par personne
-        // récupération des 3 réponses justes
-        // grâce aux clefs tu prépares la question
-        // recolle tout ça dans un tableau dans le bon sens par rapport aux besoins d'Amca
-        // json_encode
-
-        var_dump($getQuestion);
-        return $this->render('questionPage/index.html.twig', [
-            'apians' => $apiGet
-        ]);
+        return $this->render('test_Xav/index.html.twig');
     }
 }
